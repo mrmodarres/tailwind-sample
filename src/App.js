e605c4ce027b84f0e8826b5ffa47cb645aea8394
mehdi modarres <mr.modarres1994@gmail.com>
2023-03-01 13:07:57 +0330

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="relative   bg-slate-700  h-screen w-full  ">
+      <h1 className="text-3xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 transition ease-in-out duration-500 cursor-pointer  font-bold  absolute  top-1/2 right-1/2   animate-gradient  ">
+        Hello white
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      keyframes: {
+        gradient: {
+          "0%, 100%": {
+            backgroundPosition: "0% 50%",
+          },
+          "50%": {
+            backgroundPosition: "100% 50%",
+          },
+        },
+      },
+      animation: {
+        gradient: "gradient 3s ease-in-out infinite",
+      },
+    },
+  },
+  plugins: [],
+};
